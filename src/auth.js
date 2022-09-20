65f563ea54177e97c9f52d85843965cf89631106
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-09-20 16:09:44 +0400

    edit

--- src/auth.js
+++ src/auth.js
@@ -4,9 +4,9 @@ const request = ({ url, method = 'POST', token, data }) => {
   return fetch(`${BASE_URL}${url}`, {
     method,
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      ...(!!token && { 'Authorization': `Bearer ${token}` }),
+      ...(!!token && { Authorization: `Bearer ${token}` }),
     },
     ...(!!data && { body: JSON.stringify(data) }),
   }).then((res) => {
@@ -38,63 +38,3 @@ export const getContent = (token) => {
     token,
   });
 };
-
-// export const register = (email, password) => {
-//   return fetch(`${BASE_URL}/signup`, {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ email, password }),
-//   })
-//     .then((response) => {
-//       try {
-//         if (response.status === 201) {
-//           return response.json();
-//         }
-//       } catch (e) {
-//         return e;
-//       }
-//     })
-//     .then((res) => {
-//       return res;
-//     })
-//     .catch((err) => console.log(err));
-// };
-
-// export const authorize = (email, password) => {
-//   return fetch(`${BASE_URL}/signin`, {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ email, password }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       //Проверяем, есть ли свойство jwt в объекте data, который вернул сервер
-//       if (data.token) {
-//         localStorage.setItem('jwt', data.token);
-//         return data;
-//         //jwt отсутствует, ничего не вернётся и токена не будет
-//       } else {
-//         return;
-//       }
-//     })
-//     .catch((err) => console.log(err));
-// };
-
-// export const getContent = (token) => {
-//   return fetch(`${BASE_URL}/users/me`, {
-//     method: 'GET',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer ${token}`,
-//     },
-//   })
-//     .then((res) => res.json())
-//     .then((data) => data);
-// };
